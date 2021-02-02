09f642303fa7d54e04206e3c93ffd43b221f6c88
shortdaddy0711 <sodoatm@gmail.com>
2021-02-02 13:33:26 -0800

    completed testing feature

--- myapp/app_test.go
+++ myapp/app_test.go
@@ -1,9 +1,11 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,4 +37,39 @@ func TestBarPathHandler_WithoutName(t *testing.T) {
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := ioutil.ReadAll(res.Body)
 	assert.Equal("Hello World!", string(data))
+}
+
+func TestBarPathHandler_WithName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?name=namsoo", nil)
+
+	mux := NewHTTPHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello namsoo!", string(data))
+}
+
+func TestFooHandler_WithoutJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/foo",
+		strings.NewReader(
+			`{"first_name": "namsoo", "last_name": "lee", "email": "sodoatm@gmail.com"}`))
+
+	mux := NewHTTPHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	user := new(User)
+	err := json.NewDecoder(res.Body).Decode(user)
+	assert.Nil(err)
+	assert.Equal("namsoo", user.FirstName)
+	assert.Equal("lee", user.LastName)
+
 }
